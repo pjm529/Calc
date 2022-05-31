564a30c881b93fea06cab36b791d002822b7dbc0
pjm529 <pok_nima@naver.com>
2022-05-31 14:28:03 +0900

    Feat: 계산기 레이아웃 구현

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.calc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Calculator");
+
+    }
+}
