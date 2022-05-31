93f67a820ef7e39878155036fbfe26fa2b98fd89
pjm529 <pok_nima@naver.com>
2022-05-31 14:32:08 +0900

    Feat: 숫자 입력 시 Text에 숫자 append

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private double storedValue;
+    private char curOperator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Calculator");
 
+        text = findViewById(R.id.text);
+    }
+
+    public void onClick(View view) {
+        String current = text.getText().toString();
+
+        switch (view.getId()) {
+            case R.id.btn_0:
+                text.setText(current + "0");
+                break;
+            case R.id.btn_1:
+                text.setText(current + "1");
+                break;
+            case R.id.btn_2:
+                text.setText(current + "2");
+                break;
+            case R.id.btn_3:
+                text.setText(current + "3");
+                break;
+            case R.id.btn_4:
+                text.setText(current + "4");
+                break;
+            case R.id.btn_5:
+                text.setText(current + "5");
+                break;
+            case R.id.btn_6:
+                text.setText(current + "6");
+                break;
+            case R.id.btn_7:
+                text.setText(current + "7");
+                break;
+            case R.id.btn_8:
+                text.setText(current + "8");
+                break;
+            case R.id.btn_9:
+                text.setText(current + "9");
+                break;
+            case R.id.btn_dot:
+                text.setText(current + ".");
+                break;
+
+        }
     }
 }
