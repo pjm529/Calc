043465f9447aa219e1a924f8dff01fdb8dda899f
pjm529 <pok_nima@naver.com>
2022-05-31 15:30:57 +0900

    Feat: 소숫점 아래 숫자 "," 추가하지않음 및 간단한 더하기 구현

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
+    private double value1;
+    private double value2;
+    private char operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // 최대 자릿 수 지정
-        if(current.length() > 10 && view.getId() != R.id.btn_AC) {
+        if(current.length() > 10
+                && view.getId() != R.id.btn_AC
+                && view.getId() != R.id.btn_dot
+                && view.getId() != R.id.btn_eq
+                && view.getId() != R.id.btn_plus) {
             Toast.makeText(this, "최대 숫자를 넘어섰습니다.", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -73,24 +80,43 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_AC:
                 text.setText("0");
                 break;
+
+            case R.id.btn_plus:
+                value1 = Double.parseDouble(current.replaceAll(",", ""));
+                text.setText("");
+                operator = '+';
+                break;
+
+            case R.id.btn_eq:
+                value2 = Double.parseDouble(current.replaceAll(",", ""));
+                result();
+                break;
+
         }
 
         check();
     }
 
+    public void result() {
+        text.setText("" + (value1 + value2));
+    }
     // 숫자 변환기
     public void check() {
+
         String temp = text.getText().toString();
         String current;
+        int n = temp.indexOf(".");
+
+        if(n != -1){
+            temp = temp.substring(0, n);
+        }
 
         temp = temp.replaceAll(",", "");
 
         if(temp.length() > 6) {
-            System.out.println(6);
             current = temp.substring(0, temp.length() - 6) + "," + temp.substring(temp.length() - 6, temp.length() - 3) + "," + temp.substring(temp.length() - 3, temp.length());
             text.setText(current);
         } else if(temp.length() > 3) {
-            System.out.println(3);
             current = temp.substring(0, temp.length() - 3) + "," + temp.substring(temp.length() - 3, temp.length());
             text.setText(current);
         }
